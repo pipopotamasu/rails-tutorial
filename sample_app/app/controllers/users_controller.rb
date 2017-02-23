7d787db71b226ed35edc296201fcaeafa131d22e
pipopotamasu <ec0156hx@gmail.com>
2017-02-23 12:32:21 +0000

    done chapter10

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  # index, edit, updateアクションが実行される前、logged_in_userメソッドを実行
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -18,9 +27,56 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    # update_attributesメソッドの引数はキーとバリューのハッシュとなっているため、そのままで更新をかけられる
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
+    # StrongParameters
+    # テーブル更新時に、permitメソッドで許可したカラムしか更新できなくする
+    # requireはテーブルの指定
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      # unlessは真偽値が偽の時に実行される
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      # ユーザーがアクセスしようとしているURIのユーザーを取得
+      @user = User.find(params[:id])
+      # 上記のユーザーとセッションのユーザーを比較し、異なっていたらroot urlにリダイレクトさせる
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create!はcreateに失敗したら例外を投げる。createは失敗時にfalseを返す
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
